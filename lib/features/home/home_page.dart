d9abd6175ba4e01792cad79a5aaaefa84f295a28
Kurogoma939 <kan.sss.939@gmail.com>
2023-09-27 07:11:22 +0900

    homeのUI修正

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../core/router/app_router.dart';
+import '../../gen/assets.gen.dart';
 import '../component/color/color_theme.dart';
 import '../component/text/middle_headline_text.dart';
 import 'components/create_item_bottom_sheet.dart';
@@ -29,86 +30,98 @@ class HomePage extends HookConsumerWidget {
     );
 
     final state = ref.watch(homePageViewModelProvider);
-    return Scaffold(
-      body: Stack(
-        children: [
-          CustomScrollView(
-            physics: const BouncingScrollPhysics(),
-            slivers: [
-              state.recordList.isEmpty
-                  ? SliverPadding(
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      sliver: SliverToBoxAdapter(
-                        child: Center(
-                          child: MiddleHeadlineText(
-                            'レコードがありません',
-                            textColor: ColorTheme.primaryBlack,
-                          ),
-                        ),
-                      ),
-                    )
-                  : SliverList(
-                      delegate: SliverChildBuilderDelegate(
-                        (context, index) {
-                          return Card(
-                            color: ColorTheme.secondaryOrange,
-                            margin: const EdgeInsets.symmetric(
-                              horizontal: 16,
-                              vertical: 6,
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 12,
-                                vertical: 10,
-                              ),
-                              child: Text(
-                                '${state.recordList[index].category} ${state.recordList[index].load} ${state.recordList[index].weightUnitType.name} x ${state.recordList[index].time} ${state.recordList[index].rmUnitType.name}',
-                                style: const TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 15,
-                                ),
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          );
-                        },
-                        childCount: state.recordList.length,
-                      ),
-                    ),
-              SliverPadding(
-                padding: const EdgeInsets.all(16),
-                sliver: SliverToBoxAdapter(
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      await AppRouter().showBottomSheet(
-                        context,
-                        const CreateItemBottomSheet(),
-                      );
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        ColorTheme.primaryOrange,
-                      ),
-                    ),
-                    child: const Icon(
-                      Icons.add,
-                      color: Colors.black,
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            CustomScrollView(
+              physics: const BouncingScrollPhysics(),
+              slivers: [
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 36),
+                    child: SizedBox(
+                      height: 160,
+                      width: MediaQuery.sizeOf(context).width,
+                      child: Assets.images.homeHeader.image(),
                     ),
                   ),
                 ),
-              ),
-              const SliverToBoxAdapter(
-                child: SizedBox(height: 200),
-              ),
-            ],
-          ),
-          const Positioned(
-            bottom: 24,
-            right: 24,
-            child: FloatingActionButtonWidgets(),
-          ),
-        ],
+                state.recordList.isEmpty
+                    ? SliverPadding(
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                        sliver: SliverToBoxAdapter(
+                          child: Center(
+                            child: MiddleHeadlineText(
+                              'レコードがありません',
+                              textColor: ColorTheme.primaryBlack,
+                            ),
+                          ),
+                        ),
+                      )
+                    : SliverList(
+                        delegate: SliverChildBuilderDelegate(
+                          (context, index) {
+                            return Card(
+                              color: ColorTheme.secondaryOrange,
+                              margin: const EdgeInsets.symmetric(
+                                horizontal: 16,
+                                vertical: 6,
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 12,
+                                  vertical: 10,
+                                ),
+                                child: Text(
+                                  '${state.recordList[index].category} ${state.recordList[index].load} ${state.recordList[index].weightUnitType.name} x ${state.recordList[index].time} ${state.recordList[index].rmUnitType.name}',
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 15,
+                                  ),
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                            );
+                          },
+                          childCount: state.recordList.length,
+                        ),
+                      ),
+                SliverPadding(
+                  padding: const EdgeInsets.all(16),
+                  sliver: SliverToBoxAdapter(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await AppRouter().showBottomSheet(
+                          context,
+                          const CreateItemBottomSheet(),
+                        );
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          ColorTheme.primaryOrange,
+                        ),
+                      ),
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                const SliverToBoxAdapter(
+                  child: SizedBox(height: 200),
+                ),
+              ],
+            ),
+            const Positioned(
+              bottom: 24,
+              right: 24,
+              child: FloatingActionButtonWidgets(),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/gen/fonts.gen.dart
@@ -0,0 +1,27 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class FontFamily {
+  FontFamily._();
+
+  /// Font family: LINE_Seed_JP
+  static const String lINESeedJP = 'LINE_Seed_JP';
+
+  /// Font family: Noto_Sans_JP
+  static const String notoSansJP = 'Noto_Sans_JP';
+
+  /// Font family: RocknRoll_One
+  static const String rocknRollOne = 'RocknRoll_One';
+
+  /// Font family: Yusei_Magic
+  static const String yuseiMagic = 'Yusei_Magic';
+
+  /// Font family: Zen_Maru_Gothic
+  static const String zenMaruGothic = 'Zen_Maru_Gothic';
+}
