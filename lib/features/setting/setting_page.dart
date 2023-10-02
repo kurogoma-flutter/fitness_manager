10d4190b54a89f0ee4956bf55de486b849706bad
Kurogoma939 <kan.sss.939@gmail.com>
2023-10-02 23:08:52 +0900

    設定画面のUI実装

--- lib/features/setting/setting_page.dart
+++ lib/features/setting/setting_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../component/color/color_theme.dart';
+
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
 
@@ -9,25 +11,73 @@ class SettingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
+      backgroundColor: ColorTheme.primaryBackGround,
+      appBar: AppBar(
+        backgroundColor: ColorTheme.primaryBackGround,
+        title: Text(
+          '設定',
+          style: TextStyle(
+            color: ColorTheme.primaryText,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(top: 28, bottom: 40),
+          child: Column(
+            children: [
+              SettingListTile(label: 'このアプリについて'),
+              SettingListTile(label: '利用規約'),
+              SettingListTile(label: 'プライバシーポリシー'),
+              SettingListTile(label: 'ライセンス'),
+              SettingListTile(label: 'お問い合わせ'),
+              SettingListTile(label: 'データリセット'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SettingListTile extends StatelessWidget {
+  const SettingListTile({
+    required this.label,
+    super.key,
+  });
+
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 6,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ListTile(
-            title: const Text('Setting1'),
-            onTap: () {
-              Navigator.of(context).pushNamed('setting1');
-            },
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                label,
+                style: TextStyle(
+                  color: ColorTheme.primaryText,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              Icon(
+                Icons.arrow_forward_ios,
+                color: ColorTheme.primaryIcon,
+              )
+            ],
           ),
-          ListTile(
-            title: const Text('Setting2'),
-            onTap: () {
-              Navigator.of(context).pushNamed('setting2');
-            },
-          ),
-          ListTile(
-            title: const Text('Setting3'),
-            onTap: () {
-              Navigator.of(context).pushNamed('setting3');
-            },
+          Divider(
+            color: ColorTheme.primaryIcon,
           ),
         ],
       ),
