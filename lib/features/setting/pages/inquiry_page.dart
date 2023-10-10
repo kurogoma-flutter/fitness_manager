5db1d80640efb0077260ae36b04b3f2636202044
Kurogoma939 <kan.sss.939@gmail.com>
2023-10-10 23:46:03 +0900

    お問い合わせ画面の仮実装

--- lib/features/setting/pages/inquiry_page.dart
+++ lib/features/setting/pages/inquiry_page.dart
@@ -3,6 +3,12 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../core/router/app_router.dart';
 import '../../component/color/color_theme.dart';
+import '../../component/form/input_text_form.dart';
+
+final textFormKey = GlobalKey<FormState>();
+final titleFieldKey = GlobalKey<FormFieldState<String>>();
+final emailFieldKey = GlobalKey<FormFieldState<String>>();
+final contentFieldKey = GlobalKey<FormFieldState<String>>();
 
 class InquiryPage extends HookConsumerWidget {
   const InquiryPage({super.key});
@@ -32,8 +38,65 @@ class InquiryPage extends HookConsumerWidget {
         ),
         backgroundColor: ColorTheme.primaryBackGround,
       ),
-      body: const Center(
-        child: Text('このアプリは筋トレのためのアプリです。'),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: CustomScrollView(
+          slivers: [
+            SliverPadding(
+              padding: const EdgeInsets.only(top: 32),
+              sliver: SliverToBoxAdapter(
+                child: InputTextForm(
+                  textFormKey: titleFieldKey,
+                  validator: (_) => null,
+                  formHintText: 'お問い合わせやリクエストの概要',
+                ),
+              ),
+            ),
+            SliverPadding(
+              padding: const EdgeInsets.only(top: 24),
+              sliver: SliverToBoxAdapter(
+                child: InputTextForm(
+                  textFormKey: emailFieldKey,
+                  validator: (_) => null,
+                  formHintText: 'お客様のメールアドレス',
+                ),
+              ),
+            ),
+            SliverPadding(
+              padding: const EdgeInsets.only(top: 24),
+              sliver: SliverToBoxAdapter(
+                child: InputTextForm.multiLine(
+                  maxLength: 1000,
+                  textFormKey: contentFieldKey,
+                  validator: (_) => null,
+                  formHintText: 'お問い合わせやリクエストの概要',
+                ),
+              ),
+            ),
+            SliverPadding(
+              padding: const EdgeInsets.only(top: 24),
+              sliver: SliverToBoxAdapter(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: ColorTheme.primaryWhite,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                  ),
+                  child: Text(
+                    '送信する',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: ColorTheme.primaryBackGround,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
