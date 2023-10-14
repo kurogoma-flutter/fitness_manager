fdd9b0632eec70dd0b80fe2013878c20fa381562
Kurogoma939 <kan.sss.939@gmail.com>
2023-10-14 22:18:19 +0900

    不要コード整理

--- lib/initialize.dart
+++ lib/initialize.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
-import 'dart:ui';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
