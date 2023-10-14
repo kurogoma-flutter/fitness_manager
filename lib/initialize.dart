7010e1f0e3b819fcfcc54bdf8812ef95b3ef1ca7
Kurogoma939 <kan.sss.939@gmail.com>
2023-10-14 22:16:11 +0900

    firebase_crashlytics導入

--- lib/initialize.dart
+++ lib/initialize.dart
@@ -1,10 +1,15 @@
 import 'dart:async';
+import 'dart:ui';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/model/isar/isar_instance.dart';
+import 'firebase_options.dart';
 
 class Initialize {
   Future<void> init() async {
@@ -14,6 +19,9 @@ class Initialize {
     await Supabase.initialize(
       url: dotenv.env['SUPABASE_URL']!,
       anonKey: dotenv.env['SUPABASE_KEY']!,
+    );
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
     );
     await SharedPreferences.getInstance();
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -6,18 +10,30 @@ import 'features/component/dialog/unexpected_error_dialog.dart';
 import 'initialize.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // アプリ実行前の初期化処理
-  await Initialize().init();
-  // エラー発生時の処理
-  FlutterError.onError = (FlutterErrorDetails details) {
-    FlutterError.dumpErrorToConsole(details);
+  await runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    // アプリ実行前の初期化処理
+    await Initialize().init();
+    // エラー発生時の処理
+    FlutterError.onError = (FlutterErrorDetails details) {
+      FirebaseCrashlytics.instance.recordFlutterError(details);
+      FlutterError.dumpErrorToConsole(details);
+      runApp(
+        UnexpectedErrorDialog(
+          errorMessage: details.exceptionAsString(),
+        ),
+      );
+    };
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack);
+      return true;
+    };
     runApp(
-      UnexpectedErrorDialog(
-        errorMessage: details.exceptionAsString(),
+      const ProviderScope(
+        child: Application(),
       ),
     );
-  };
-  // アプリ実行
-  runApp(const ProviderScope(child: Application()));
+  }, (error, stackTrace) {
+    FirebaseCrashlytics.instance.recordError(error, stackTrace);
+  });
 }
